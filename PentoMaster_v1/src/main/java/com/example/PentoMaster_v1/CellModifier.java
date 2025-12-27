5662609d1d353964d20d8717b36aad84735563a9
Solo0l <151218749+Solo0l@users.noreply.github.com>
2025-12-27 05:40:44 +0300

    Delete PentoMaster_v1 directory

--- PentoMaster_v1/src/main/java/com/example/PentoMaster_v1/CellModifier.java
+++
@@ -1,8 +0,0 @@
-package com.example.PentoMaster_v1;
-
-public enum CellModifier {
-    NONE,
-    CRACKED,  // تختفي الخلية بعد محاولتين وضع
-    FROZEN,   // تمنع الوضع لمدة 5 ثواني إذا فعلت
-    VOLATILE  // تضاعف خسارة الـ Stability (العداد الأحمر)
-}
